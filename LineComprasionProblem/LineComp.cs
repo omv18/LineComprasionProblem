856f99cea3b765bcc8c83046b34b6219ccc09198
sOny@4981 <18cse034.souravkumar@giet.edu>
2022-10-28 09:08:31 +0530

    [sourav]add : UC1-calculate

---
+++ LineComprasionProblem/LineComp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComprasionProblem
+{
+    internal class LineComp
+    {
+        int x1, y1, x2, y2;
+        public LineComp(int x1, int y1,int x2 ,int y2)
+        {
+            this.x1 = x1;   
+            this.y1 = y1;   
+            this.x2 = x2;
+            this.y2 = y2;   
+        }
+        public void Calculate()
+        {
+            double cal = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2));
+            Console.WriteLine("Length of the line is : " + cal);
+        }
+    }
+}
--- LineComprasionProblem/Program.cs
+++ LineComprasionProblem/Program.cs
@@ -6,6 +6,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the line comparision problrm statement");
+            LineComp line = new LineComp(3, 4, -2, 3);
+            line.Calculate();
         }
     }
 }
